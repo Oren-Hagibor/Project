d42e81974cc6c30ff6a98ede857c046ee99f457d
roynovich1451 <57090297+roynovich1451@users.noreply.github.com>
2020-02-28 18:37:00 +0200

    updated login
    login on enter added

--- js/login.js
+++ js/login.js
@@ -1,25 +1,32 @@
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
 	if (user) {		// User is signed in
-		document.getElementById("login_div").style.display = "none";		
-		var user = firebase.auth().currentUser;		
-		if(user != null){
+		document.getElementById("login_div").style.display = "none";
+		var user = firebase.auth().currentUser;
+		if (user != null) {
 			window.location.replace("calendar.html");
-		}		
-		} else {
+		}
+	} else {
 		// No user is signed in.
-		
-		document.getElementById("login_div").style.display = "block";		
+
+		document.getElementById("login_div").style.display = "block";
 	}
 });
 
-function login(){	
+function login() {
 	var userEmail = document.getElementById("email_field").value;
 	var userPass = document.getElementById("password_field").value;
-	
-	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
 		// Handle Errors here.
 		var errorCode = error.code;
-		var errorMessage = error.message;		
+		var errorMessage = error.message;
 		window.alert("Error : " + errorMessage);
 	});
 }
+
+document.addEventListener("keyup", function (event) {
+	if (event.keyCode === 13) {
+		login();
+	}
+});
+
